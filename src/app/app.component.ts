86aa83b85fd9f4be68a5c2f09750582a8c6836be
vijaykumar <vijay1111mane@gmail.com>
2022-05-07 18:28:31 +0530

    commit;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,7 +12,24 @@ export class AppComponent {
   registrationForm = new FormGroup({
     userName: new FormControl('Vijay'),
     password: new FormControl(''),
-    confirmPassword: new FormControl('')
+    confirmPassword: new FormControl(''),
+    address: new FormGroup({
+      city: new FormControl(''),
+      state: new FormControl(''),
+      postalCode: new FormControl('')
+    })
   });
+  loadApiData(){
+    this.registrationForm.setValue({
+      userName:'kumar',
+      password:'test',
+      confirmPassword:'test',
+      address:{
+        city: 'City',
+        state:'State',
+        postalCode:'123456'
+      }
+    });
+  }
 }
 
